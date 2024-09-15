49c0df0a3e090eec0eadbfd408dd27e540687416
devipriyal <devipriyal.vanangamudi@byteforza.com>
2024-09-15 19:16:41 +0530

    added method

--- src/test/java/Repository/googlePageRepo.java
+++ src/test/java/Repository/googlePageRepo.java
@@ -1,7 +1,9 @@
 package Repository;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
@@ -16,6 +18,14 @@ public class googlePageRepo {
 		this.driver=driver;
 		PageFactory.initElements(driver, this);
 		
+	}
+	
+	public void enterSearchText() throws InterruptedException{
+		searchTextBox.sendKeys("Amazon");
+		Actions act= new Actions(driver);
+		act.keyDown(Keys.ENTER);
+		act.keyUp(Keys.ENTER);
+		Thread.sleep(1000);
 	}
 
 }
