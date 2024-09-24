704c22aa2d8f9dd15af7f088b87b3e9a4a73c4d9
devipriyal <devipriyal.vanangamudi@byteforza.com>
2024-09-24 22:05:32 +0530

    made changes

--- src/main/java/Utility/driverInit.java
+++ src/main/java/Utility/driverInit.java
@@ -1,14 +1,25 @@
 package Utility;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeTest;
+
+
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class driverInit {
 	
 	WebDriver driver;
+	
+	
 	public WebDriver InitialiseDriver(String browser){
 		browser=browser.toLowerCase();
 		if(browser!=null){
@@ -37,6 +48,7 @@ public class driverInit {
 		driver.manage().window().maximize();
 		return driver;	
 	}
+	
 	
 
 }
--- src/test/java/Repository/googlePageRepo.java
+++ src/test/java/Repository/googlePageRepo.java
@@ -1,5 +1,6 @@
 package Repository;
 
+import org.omg.CORBA.TIMEOUT;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -7,6 +8,10 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+
 
 public class googlePageRepo {
 	WebDriver driver;
@@ -17,15 +22,16 @@ public class googlePageRepo {
 	public googlePageRepo(WebDriver driver) {
 		this.driver=driver;
 		PageFactory.initElements(driver, this);
-		
 	}
 	
 	public void enterSearchText() throws InterruptedException{
-		searchTextBox.sendKeys("Amazon");
+		Thread.sleep(1000);
+		if(searchTextBox.isEnabled() && searchTextBox.isDisplayed()){
+		searchTextBox.sendKeys("Amazon");;
 		Actions act= new Actions(driver);
 		act.keyDown(Keys.ENTER);
 		act.keyUp(Keys.ENTER);
-		Thread.sleep(1000);
+		}
 	}
 
 }
